96f8f55c16cfebacf020148c3d0558adeb0cdc51
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 18:08:03 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,11 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_12_lambda;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese un número: ");
+        int n = sc.nextInt();
+        System.out.println("El número de Fibonacci de " + n + " es: " + ej2_12_lambda.fibonacci(n));
+    }
 }
--- src/dominio/ej2_12_lambda.java
+++ src/dominio/ej2_12_lambda.java
@@ -1,2 +1,8 @@
-package dominio;public class ej2_12_lambda {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej2_12_lambda {
+    public static int fibonacci(int n){
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]}).limit(n).map(f -> f[0]).reduce(0, (a, b) -> b);
+    }
 }
